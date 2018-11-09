2a05e580bbeb37cd78429662e72f032c7f416eb8
DAW <DAW@192.168.87.30>
2018-11-09 16:33:07 +0100

    Introducido valor generado aleatoriamente

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        $aleatorio = rand(1,10);
+        echo $aleatorio;
         ?>
     </body>
 </html>
